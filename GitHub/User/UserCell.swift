d78d895b3adc2dfdd87461514ea8b13596654511
Wasith Theerapattrathamrong <wasith.t@gmail.com>
2020-04-12 13:14:26 +0700

    Set value to user cell

--- GitHub/User/UserCell.swift
+++ GitHub/User/UserCell.swift
@@ -10,6 +10,15 @@ class UserCell: UITableViewCell {
   @IBOutlet weak var favoriteButton: UIButton!
 
   @IBAction func favoriteButtonTapped(_ sender: Any) {
+    // TODO: Favorite and unfavorite
+  }
+
+  func configureWith(user: User) {
+    // TODO: Set avatar image
+    usernameLabel.text = user.login
+    urlLabel.text = user.html_url
+    accountTypeLabel.text = user.type
+    adminStatusLabel.text = String(user.site_admin)
   }
 
   static var reusableIdentifier: String {
