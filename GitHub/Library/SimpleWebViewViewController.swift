5a8bddfbccadab2e92d9e836dfbf2de7c7a62c9e
Wasith Theerapattrathamrong <wasith.t@gmail.com>
2020-04-13 02:36:44 +0700

    Add close button

--- GitHub/Library/SimpleWebViewViewController.swift
+++ GitHub/Library/SimpleWebViewViewController.swift
@@ -11,9 +11,34 @@ class SimpleWebViewViewController: UIViewController, WKNavigationDelegate {
     view = webView
   }
 
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setupToolBar()
+  }
+
   func configureWith(url: String) {
     let url = URL(string: url)!
     webView.load(URLRequest(url: url))
   }
 
+  fileprivate func setupToolBar() {
+    let closeButton = UIBarButtonItem(title: "Close", style: .plain, target: self, action: #selector(dismiss(_:)))
+    let toolBar = UIToolbar()
+    toolBar.isTranslucent = false
+    toolBar.translatesAutoresizingMaskIntoConstraints = false
+    toolBar.items = [closeButton]
+    webView.addSubview(toolBar)
+
+    let constraints = [
+      toolBar.bottomAnchor.constraint(equalTo: webView.layoutMarginsGuide.bottomAnchor, constant: 0),
+      toolBar.leadingAnchor.constraint(equalTo: webView.leadingAnchor, constant: 0),
+      toolBar.trailingAnchor.constraint(equalTo: webView.trailingAnchor, constant: 0)
+    ]
+
+    NSLayoutConstraint.activate(constraints)
+  }
+
+  @objc private func dismiss(_ sender: Any) {
+    dismiss(animated: true, completion: nil)
+  }
 }
