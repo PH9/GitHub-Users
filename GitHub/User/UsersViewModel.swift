67ed028b5a1c6242dcc50be6904c100a5d31be6e
Wasith Theerapattrathamrong <wasith.t@gmail.com>
2020-04-12 20:05:44 +0700

    Use WebService and UsersRequest

--- GitHub/User/UsersViewModel.swift
+++ GitHub/User/UsersViewModel.swift
@@ -11,7 +11,9 @@ class UsersViewModel {
 
   func getUsers(sinceId userId: Int = 0) {
     isFetching = true
-    getUsers(sinceId: userId) { result in
+
+    let userRequest = UsersRequest(sinceId: userId)
+    WebService.shared.request(request: userRequest) { result in
       defer {
         self.isFetching = false
       }
@@ -24,33 +26,6 @@ class UsersViewModel {
         self.getUserFailure?(error)
       }
     }
-  }
-
-  private func getUsers(sinceId userId: Int,
-                        completionHandler completion: @escaping (Result<[User], AppError>) -> Void) {
-    let session = URLSession.shared
-    let url = URL(string: "https://api.github.com/users?since=\(userId)")!
-
-    let task = session.dataTask(with: url) { data, _, error in
-      if let error = error {
-        let appError = AppError(error: error)
-        completion(.failure(appError))
-        return
-      }
-
-      guard
-        let data = data,
-        let users = try? JSONDecoder().decode([User].self, from: data)
-        else {
-          let appError = AppError(message: "Unexpected response, please try again later")
-          completion(.failure(appError))
-          return
-      }
-
-      completion(.success(users))
-    }
-
-    task.resume()
   }
 
 }
