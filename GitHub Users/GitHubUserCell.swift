cfdc652307a90c0694ade48474fa385fc0082986
Wasith Theerapattrathamrong <wasith.t@gmail.com>
2020-04-12 12:07:09 +0700

    Link GitHubUserCell between Storyboard and Code

---
+++ GitHub Users/GitHubUserCell.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+class GitHubUserCell: UITableViewCell {
+
+  @IBOutlet weak var avatarImage: UIImageView!
+  @IBOutlet weak var usernameLabel: UILabel!
+  @IBOutlet weak var urlLabel: UILabel!
+  @IBOutlet weak var accountTypeLabel: UILabel!
+  @IBOutlet weak var adminStatusLabel: UILabel!
+  @IBOutlet weak var favoriteButton: UIButton!
+
+  @IBAction func favoriteButtonTapped(_ sender: Any) {
+  }
+
+}
