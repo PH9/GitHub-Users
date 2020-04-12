ce8ec60c8db7d9d029c36fca4458c6323d810718
Wasith Theerapattrathamrong <wasith.t@gmail.com>
2020-04-12 12:19:15 +0700

    Draft GetUsers function

--- GitHub Users/GitHubUserViewController.swift
+++ GitHub Users/GitHubUserViewController.swift
@@ -2,5 +2,20 @@ import UIKit
 
 class GitHubUserViewController: UITableViewController {
 
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    getUsers { result in
+      switch result {
+      case .success(let user):
+        print(user)
+      case .failure(let error):
+        print(error)
+      }
+    }
+  }
+
+  private func getUsers(completionHandler completion: @escaping (Result<User, Error>) -> Void) {
+
+  }
 }
 
