b779a0208156b95753293a0df67b1d3d834b0b56
Wasith Theerapattrathamrong <wasith.t@gmail.com>
2020-04-12 11:54:47 +0700

    Cleanup code remove optional function that is not custom yet, it make noise in project

--- GitHub Users/ViewController.swift
+++ GitHub Users/ViewController.swift
@@ -1,20 +1,6 @@
-//
-//  ViewController.swift
-//  GitHub Users
-//
-//  Created by Wasith Theerapattrathamrong on 12/4/2563 BE.
-//  Copyright © 2563 Wasith Theerapattrathamrong. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view.
-  }
-
 
 }
 
