d7901e3fe9f8af12d61abe6aae5de46f42f00140
Wasith Theerapattrathamrong <wasith.t@gmail.com>
2020-04-12 20:03:35 +0700

    Copy WebService.request from UserViewModel.getUsers

--- GitHub/Library/WebService.swift
+++ GitHub/Library/WebService.swift
@@ -2,7 +2,7 @@ import Foundation
 
 protocol Requestable {
 
-  associatedtype ResponseType
+  associatedtype ResponseType: Decodable
 
   var url: URL { get }
 }
@@ -13,7 +13,28 @@ class WebService {
 
   func request<R: Requestable>(
     request: R,
-    completionHandler completion: (Result<R.ResponseType, AppError>) -> Void) {
-    
+    completionHandler completion: @escaping (Result<R.ResponseType, AppError>) -> Void) {
+    let session = URLSession.shared
+
+    let task = session.dataTask(with: request.url) { data, _, error in
+      if let error = error {
+        let appError = AppError(error: error)
+        completion(.failure(appError))
+        return
+      }
+
+      guard
+        let data = data,
+        let decoded = try? JSONDecoder().decode(R.ResponseType.self, from: data)
+        else {
+          let appError = AppError(message: "Unexpected response, please try again later")
+          completion(.failure(appError))
+          return
+      }
+
+      completion(.success(decoded))
+    }
+
+    task.resume()
   }
 }
