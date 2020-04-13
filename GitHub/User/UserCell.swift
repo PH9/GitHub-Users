3066f83ba131cd02820d9135d0361c0b2c19dde2
Wasith Theerapattrathamrong <wasith.t@gmail.com>
2020-04-13 11:50:07 +0700

    Fix constraint change botton to icon, tint color by status

--- GitHub/User/UserCell.swift
+++ GitHub/User/UserCell.swift
@@ -12,7 +12,6 @@ class UserCell: UITableViewCell {
   @IBOutlet weak var favoriteButton: UIButton!
 
   @IBOutlet var regularConstraints: [NSLayoutConstraint]!
-  @IBOutlet var commonConstrains: [NSLayoutConstraint]!
   private var largeTextConstraints: [NSLayoutConstraint] = []
   private var user: User!
 
@@ -24,33 +23,13 @@ class UserCell: UITableViewCell {
 
   private func createConstrantsForAccessibilityCategory() {
     largeTextConstraints = [
-      avatarImage.topAnchor.constraint(equalTo: contentView.layoutMarginsGuide.topAnchor),
-      avatarImage.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor),
       avatarImage.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor),
-
-      usernameLabel.topAnchor.constraint(equalTo: avatarImage.bottomAnchor, constant: 8),
-      usernameLabel.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor, constant: 0),
-      usernameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: 8),
-
-      urlLabel.topAnchor.constraint(equalTo: usernameLabel.bottomAnchor, constant: 8),
-      urlLabel.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor),
-      urlLabel.trailingAnchor.constraint(equalTo: usernameLabel.trailingAnchor),
-
-      accountTypeHeaderLabel.topAnchor.constraint(equalTo: urlLabel.bottomAnchor, constant: 8),
-      accountTypeHeaderLabel.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor),
-      accountTypeHeaderLabel.trailingAnchor.constraint(equalTo: urlLabel.trailingAnchor),
-
-      accountTypeLabel.topAnchor.constraint(equalTo: accountTypeHeaderLabel.bottomAnchor, constant: 8),
-      accountTypeLabel.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor),
-      accountTypeLabel.trailingAnchor.constraint(equalTo: accountTypeHeaderLabel.trailingAnchor),
-
-      adminStatusHeaderLabel.topAnchor.constraint(equalTo: accountTypeLabel.bottomAnchor, constant: 8),
-      adminStatusHeaderLabel.leadingAnchor.constraint(equalTo: accountTypeLabel.leadingAnchor),
-      adminStatusHeaderLabel.trailingAnchor.constraint(equalTo: accountTypeLabel.trailingAnchor),
-
-      adminStatusLabel.topAnchor.constraint(equalTo: adminStatusHeaderLabel.bottomAnchor, constant: 8),
-      adminStatusLabel.leadingAnchor.constraint(equalTo: adminStatusHeaderLabel.leadingAnchor),
-      adminStatusLabel.trailingAnchor.constraint(equalTo: adminStatusHeaderLabel.trailingAnchor)
+      usernameLabel.topAnchor.constraint(equalTo: avatarImage.bottomAnchor, constant: 4),
+      usernameLabel.leadingAnchor.constraint(equalTo: avatarImage.leadingAnchor),
+      accountTypeLabel.topAnchor.constraint(equalTo: accountTypeHeaderLabel.bottomAnchor, constant: 4),
+      accountTypeLabel.leadingAnchor.constraint(equalTo: accountTypeHeaderLabel.leadingAnchor),
+      adminStatusLabel.topAnchor.constraint(equalTo: adminStatusHeaderLabel.bottomAnchor, constant: 4),
+      adminStatusLabel.leadingAnchor.constraint(equalTo: adminStatusHeaderLabel.leadingAnchor)
     ]
   }
 
@@ -71,8 +50,11 @@ class UserCell: UITableViewCell {
 
   private func setButtonTitleBy(userId: Int) {
     let isFavorite = UserDefaults.standard.bool(forKey: generateFavoriteKey(userId: user.id))
-    let buttonTitle = getButtonTitle(isFavorite: isFavorite)
-    favoriteButton.setTitle(buttonTitle, for: .normal)
+    favoriteButton.setTitle("", for: .normal)
+    let heart = UIImage(named: "Heart")!.withRenderingMode(.alwaysTemplate)
+    favoriteButton.setImage(heart, for: .normal)
+    let color: UIColor = isFavorite ? .systemRed : .systemGray
+    favoriteButton.tintColor = color
   }
 
   private func toggleFavoriteBy(userId: Int) {
@@ -85,17 +67,11 @@ class UserCell: UITableViewCell {
     return "favorite-user-id-\(userId)"
   }
 
-  private func getButtonTitle(isFavorite: Bool) -> String {
-    return isFavorite ? "Unfavorite" : "Favorite"
-  }
-
   override func setSelected(_ selected: Bool, animated: Bool) {
     super.setSelected(false, animated: animated)
   }
 
   private func updateLayoutConstraints() {
-    NSLayoutConstraint.activate(commonConstrains)
-
     if traitCollection.preferredContentSizeCategory.isAccessibilityCategory {
       NSLayoutConstraint.deactivate(regularConstraints)
       NSLayoutConstraint.activate(largeTextConstraints)
